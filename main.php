b481a3eca10e9fc6b78741b22d4d71bf546a13fc
Ludovicmanga <ludovic.mangaj@gmail.com>
2024-02-07 17:57:27 +0100

    transition to php

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+
+ include("server.php");
+
+ ?>
+
+ <html>
+
+   <head>
+     <meta charset="utf-8" />
+   </head>
+
+   <body>
+       
+   </body>
+
+  <script src="main.js"></script>
+    
+</html>
+
---
+++ server.php
@@ -0,0 +1,31 @@
+<?php 
+
+  echo "whatsup";
+
+  define('DB_SERVER', 'localhost:3306');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', '');
+  define('DB_DATABASE', 'mlmaths');
+  $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+
+  // Check for connection errors
+  if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // SQL query to insert a user
+  $sql = "INSERT INTO users (mail) VALUES ('test@gmail.com')";
+
+  // Execute the query
+  if (mysqli_query($db, $sql)) {
+    echo "User inserted successfully.";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+  }
+
+  // Close the database connection
+  mysqli_close($db);
+
+
+ ?>
